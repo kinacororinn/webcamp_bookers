8eb691989fbfbfc1715b8696bcc8f782bd76003e
kinacororinn <kinacororinn@gmail.com>
2021-04-22 16:27:19 +0000

    課題2再度作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:success] = 'Book was successfully updated.'
+    redirect_to book_path(@book.id)
+    else
+    render:edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    @book.destroy  # データ（レコード）を削除
+    redirect_to books_path
+  end
+
+  def create
+    @book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+    flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
+  end
+  def edit
+    @book = Book.find(params[:id])
+  end
+    private
+  # ストロングパラメータ
+   def book_params
+    params.require(:book).permit(:title, :body)
+   end
+end
